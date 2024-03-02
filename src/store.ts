fb432427418897acc62ee8c60808ec272accee03
Malik Soltukiev <malSolt99@gmail.com>
2024-03-02 17:17:27 +0100

    a little chages

--- src/store.ts
+++ src/store.ts
@@ -25,7 +25,26 @@ const createBoard = () => {
 const board = createBoard()
 
 //create pieces initial state
-const pieces: Record<string, Pawn | Knight | King | Queen> = {
+const blackPieces = {
+  '1,7': new Pawn({ x: 1, y: 7 }),
+  '2,7': new Pawn({ x: 2, y: 7 }),
+  '3,7': new Pawn({ x: 3, y: 7 }),
+  '4,7': new Pawn({ x: 4, y: 7 }),
+  '5,7': new Pawn({ x: 5, y: 7 }),
+  '6,7': new Pawn({ x: 6, y: 7 }),
+  '7,7': new Pawn({ x: 7, y: 7 }),
+  '8,7': new Pawn({ x: 8, y: 7 }),
+  '2,8': new Knight({ x: 2, y: 8 }),
+  '7,8': new Knight({ x: 7, y: 8 }),
+  '5,8': new King({ x: 5, y: 8 }),
+  '4,8': new Queen({ x: 4, y: 8 }),
+  '1,8': new Rook({ x: 1, y: 8 }),
+  '8,8': new Rook({ x: 8, y: 8 }),
+  '3,8': new Bishop({ x: 3, y: 8 }),
+  '6,8': new Bishop({ x: 6, y: 8 }),
+}
+
+const whitePieces = {
   '1,2': new Pawn({ x: 1, y: 2 }),
   '2,2': new Pawn({ x: 2, y: 2 }),
   '3,2': new Pawn({ x: 3, y: 2 }),
@@ -42,6 +61,11 @@ const pieces: Record<string, Pawn | Knight | King | Queen> = {
   '8,1': new Rook({ x: 8, y: 1 }),
   '3,1': new Bishop({ x: 3, y: 1 }),
   '6,1': new Bishop({ x: 6, y: 1 }),
+}
+
+const pieces: Record<string, Pawn | Knight | King | Queen | Rook | Bishop> = {
+  ...blackPieces,
+  ...whitePieces,
 }
 
 //create store
